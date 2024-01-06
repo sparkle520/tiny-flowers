cedda103823026d629507d9e039ecd61040e4194
sparkle520 <949784350@qq.com>
2024-01-07 02:23:40 +0800

    Changes to be committed: 	deleted:    docs/assets/AboutMe-28fe613c.js 	new file:   docs/assets/AboutMe-3d050f90.js 	deleted:    docs/assets/MessageBoard-58a91817.js 	new file:   docs/assets/MessageBoard-6bc2c629.js 	deleted:    docs/assets/UpdateLog-1f7131ba.js 	new file:   docs/assets/UpdateLog-d59f3b82.js 	renamed:    docs/assets/index-1ba81b78.js -> docs/assets/index-78eef8af.js 	deleted:    docs/assets/unknownWorldMap-0250059d.js 	new file:   docs/assets/unknownWorldMap-3b96ca21.css 	deleted:    docs/assets/unknownWorldMap-4ce64df3.css 	new file:   docs/assets/unknownWorldMap-764e5aa5.js 	modified:   docs/index.html 	modified:   src/assets/css/constant.scss 	new file:   src/component/PageBar.vue 	modified:   src/views/unknownWorldMap.vue

--- src/views/unknownWorldMap.vue
+++ src/views/unknownWorldMap.vue
@@ -3,11 +3,15 @@
 <!-- @Description:  -->
 
 <script setup>
-import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+import { reactive, ref, toRefs, onBeforeMount, onMounted } from "vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
 onBeforeMount(() => {});
 onMounted(() => {});
+const data = ref({
+  topic_count: 1,
+  visit_count: 1,
+});
 </script>
 <template>
   <div id="main">
@@ -17,11 +21,25 @@ onMounted(() => {});
       UNKNOWN<br />WORLD<br />MAP
       <div class="rectangle_1"></div>
       <div class="message">
-        If only our voices speak at night.<br>
-Could ever reach the very edge of this world and the time<br>
-Instead of fading into air and dust.<br>
-Then what will the words could ever be<br>
-For all these words from probably.
+        If only our voices speak at night.<br />
+        Could ever reach the very edge of this world and the time<br />
+        Instead of fading into air and dust.<br />
+        Then what will the words could ever be<br />
+        For all these words from probably.
+      </div>
+    </div>
+    <div class="statistics_box flex flex_direction_row justify_content_center relative">
+      <div class="topic_count flex flex_direction_column align_items_center">
+        <p>文章</p>
+        {{ data.topic_count }}
+      </div>
+      <div class="absolute rectangle_2">
+      </div>
+      <div class="visit_count flex flex_direction_column align_items_center">
+        <p>访问</p>
+        {{ data.visit_count }}
+      </div>
+      <div class="content">
       </div>
     </div>
   </div>
@@ -45,13 +63,41 @@ For all these words from probably.
       background: #262220;
       border-radius: 10px;
     }
-    .message{
-        margin-top: 10px;
-        font-family:'Microsoft YaHei';
-        font-size: 15px;
-        line-height: 20px;
+    .message {
+      margin-top: 10px;
+      font-family: "Microsoft YaHei";
+      font-size: 15px;
+      line-height: 20px;
     }
-    
+  }
+  .statistics_box {
+    width: 100%;
+    font-family: "Microsoft YaHei";
+    font-size: 30px;
+    font-weight: 800;
+    margin-top: 20px;
+    color: #f0681e;
+    p{
+      color: #705547;
+    }
+    .topic_count{
+      margin-right: 150px;
+      margin-bottom: 30px;
+    }
+    .rectangle_2{
+      width: 10px;
+      height: 100%;
+      left: 50%;
+      background-color: #262220;
+      transform: translateX(-50%);
+      border-radius: 10px;
+
+    }
+    .visit_count{
+      margin-left: 150px;
+      margin-bottom: 30px;
+
+    }
   }
 }
 </style>
